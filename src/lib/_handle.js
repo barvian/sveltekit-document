566c1c12af01baaecdc6ba085bfa2cdb93ca5022
Maxwell Barvian <max@barvian.me>
2023-02-23 21:26:08 -0600

    Switch to lighter html5parser

--- src/lib/_handle.js
+++ src/lib/_handle.js
@@ -1,4 +1,4 @@
-import { load } from 'cheerio'
+import { parse, walk, SyntaxKind } from 'html5parser'
 import { mergeAttributes } from './_util.js'
 
 /**
@@ -9,21 +9,43 @@ export default ({ event, resolve }) =>
 		async transformPageChunk({ html, done }) {
 			if (!done || !/%ska.html.attributes%|%ska.body.attributes%/.test(html)) return html
 
-			const $ = load(html)
-			/** @param {string} el */
-			const getAttributesString = (el) => {
-				const attrs = mergeAttributes(
-					$('[data-ska-document-element="' + el + '"]')
-						.toArray()
-						.map((el) => /** @type {import('cheerio').Element} */ (el).attribs)
-				)
-				return Object.entries(attrs)
-					.map(([attr, val]) => attr + '="' + val + '"')
-					.join(' ')
-			}
+			const ast = parse(html, { setAttributeMap: true })
+			/** @type {Record<string, import('html5parser').ITag[]>} */
+			let nodes = {}
+			walk(ast, {
+				enter(node) {
+					if (
+						node.type !== SyntaxKind.Tag ||
+						!node.attributeMap?.['data-ska-document-element']?.value
+					)
+						return
+					const elem = node.attributeMap['data-ska-document-element'].value.value
+					if (!nodes[elem]) nodes[elem] = []
+					nodes[elem].push(node)
+				}
+			})
+
+			const rawAttributes = Object.fromEntries(
+				Object.entries(nodes).map(([tag, nodes]) => {
+					const merged = mergeAttributes(
+						nodes.map((node) =>
+							Object.fromEntries(
+								Object.entries(node.attributeMap ?? {}).map(([name, attr]) => [
+									name,
+									attr.value?.value ?? ''
+								])
+							)
+						)
+					)
+					const raw = Object.entries(merged)
+						.map(([attr, val]) => attr + '="' + val + '"')
+						.join(' ')
+					return [tag, raw]
+				})
+			)
 
 			return html
-				.replace('%ska.html.attributes%', getAttributesString('html'))
-				.replace('%ska.body.attributes%', getAttributesString('body'))
+				.replace('%ska.html.attributes%', rawAttributes.html ?? '')
+				.replace('%ska.body.attributes%', rawAttributes.body ?? '')
 		}
 	})
